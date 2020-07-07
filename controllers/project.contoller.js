3f8961c6d1e54a9b19f2d3e440872647cada81ea
kealanheena <kealanheena@gmail.com>
2020-07-07 20:38:04 +0100

    Chore: make createProject function send a 201 status code

--- controllers/project.contoller.js
+++ controllers/project.contoller.js
@@ -2,4 +2,5 @@ const ProjectModel = require("../model/project.model");
 
 exports.createProject = (req, res, next) => {
   ProjectModel.create(req.body);
+  res.send().status(201);
 };
--- tests/unit/project.controller.test.js
+++ tests/unit/project.controller.test.js
@@ -6,19 +6,33 @@ const newProject = require("../mock-data/new-project.json");
 ProjectModel.create = jest.fn();
 
 describe("ProjectController", () => {
+
+  let req, res, next;
+
+  beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+  });
+
   describe(".createProject", () => {
     it("should have a createProject function", () => {
       expect(typeof ProjectController.createProject).toBe("function");
     });
 
     it("should call ProjectModel.create", () => {
-      let req, res, next;
-      req = httpMocks.createRequest();
-      res = httpMocks.createResponse();
-      next = null;
       req.body = newProject;
       ProjectController.createProject(req, res, next);
+
       expect(ProjectModel.create).toBeCalledWith(newProject);
     });
+
+    it("should return a 201 response code", () => {
+      req.body = newProject;
+      ProjectController.createProject(req, res, next);
+
+      expect(res.statusCode).toBe(201);
+      expect(res._isEndCalled()).toBeTruthy()
+    })
   });
 })
