8c6552f6546ea8ceaa223f2ca929121141a40f25
kealanheena <kealanheena@gmail.com>
2020-07-16 22:43:40 +0100

    Chore: make App render a div

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  let wrapper;
+
+  beforeEach(() => { wrapper = shallow(<App/>) });
+
+  it("should render a <div />", () => {
+    expect(wrapper.find("div").length).toBe(1);
+  });
 });
+
