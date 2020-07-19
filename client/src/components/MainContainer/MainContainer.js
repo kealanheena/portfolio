b8d53a3314da9b76f09f01b71dfb9ddb29067002
kealanheena <kealanheena@gmail.com>
2020-07-19 21:52:28 +0100

    Chore: add bootstrap to Main container

--- client/src/components/MainContainer/MainContainer.js
+++ client/src/components/MainContainer/MainContainer.js
@@ -4,6 +4,16 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const MainContainer = () => (
   <div>
+    <Container>
+      <Row>
+        {projects.map( data => (
+          <Col xs={3} className="mb-5" key={`${data.id}`}>
+            <ProjectCard data={data} />
+          </Col>
+        )
+        )}
+      </Row>
+    </Container>
   </div>
 )
 
