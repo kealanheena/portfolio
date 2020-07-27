087c71a4982b69db96db867802d30d541c78833c
kealanheena <kealanheena@gmail.com>
2020-07-27 22:01:01 +0100

    Chore: add snapshot test for app.js

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import App from './App';
 import MyNavbar from '../MyNavbar/MyNavbar';
 import MainContainer from '../MainContainer/MainContainer';
@@ -8,6 +9,8 @@ describe("App", () => {
   let wrapper;
 
   beforeEach(() => { wrapper = shallow(<App/>) });
+
+  it("should render correctly", () => expect(wrapper).toMatchSnapshot());
 
   it("should render a <div />", () => {
     expect(wrapper.find("div").length).toBe(1);
