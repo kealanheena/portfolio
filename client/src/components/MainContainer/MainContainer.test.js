3d0002eb3ae2a76619f5c9033a140d37df1ebdb1
kealanheena <kealanheena@gmail.com>
2020-07-28 16:44:28 +0100

    Chore: add ProjectContainer component ti MainContainer Component

--- client/src/components/MainContainer/MainContainer.test.js
+++ client/src/components/MainContainer/MainContainer.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import MainContainer from './MainContainer';
 import Profile from '../Profile/Profile';
 import ExperienceContainer from '../Experience/ExperienceContainer/ExperienceContainers';
+import ProjectContainer from '../Projects/ProjectsContainer/ProjectsContainer';
 
 describe("MainContainer", () => {
   let wrapper;
@@ -21,7 +22,12 @@ describe("MainContainer", () => {
     expect(wrapper.containsMatchingElement(<Profile />)).toEqual(true);
   });
 
-  it("should render a ExperienceContainer component", () => {
-    expect(wrapper.containsMatchingElement(<ExperienceContainer />))
-  })
+  it("should render an ExperienceContainer component", () => {
+    expect(wrapper.containsMatchingElement(<ExperienceContainer />)).toEqual(true)
+  });
+
+  it("should render a ProjectContainer component", () => {
+    expect(wrapper.containsMatchingElement(<ProjectContainer />)).toEqual(true)
+  });
+  
 });
