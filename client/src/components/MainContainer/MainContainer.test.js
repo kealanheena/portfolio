fe21b9ca1808419ba244175e56cd1c5909e2e76c
kealanheena <kealanheena@gmail.com>
2020-07-29 19:21:24 +0100

    Chore: add HobbiesContainer component to MainContainer Component

--- client/src/components/MainContainer/MainContainer.test.js
+++ client/src/components/MainContainer/MainContainer.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import MainContainer from './MainContainer';
 import Profile from '../Profile/Profile';
 import ExperienceContainer from '../Experience/ExperienceContainer/ExperienceContainers';
 import ProjectContainer from '../Projects/ProjectsContainer/ProjectsContainer';
 import EducationContainer from '../Education/EducationContainer/EducationContainer';
 import SkillsContainer from '../Skills/SkillsContainer/SkillsContainer';
+import HobbiesContainer from '../Hobbies/HobbiesContainer/HobbiesContainer';
 
 describe("MainContainer", () => {
   let wrapper;
@@ -38,6 +40,10 @@ describe("MainContainer", () => {
 
   it("should render a SkillsContainer component", () => {
     expect(wrapper.containsMatchingElement(<SkillsContainer />)).toEqual(true);
+  });
+
+  it("should render a HobbiesContainer component", () => {
+    expect(wrapper.containsMatchingElement(<HobbiesContainer />)).toEqual(true);
   });
   
 });
