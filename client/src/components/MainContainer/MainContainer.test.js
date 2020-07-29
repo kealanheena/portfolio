7959594b86bc5de83ba9f9e755e3d8233a126d19
kealanheena <kealanheena@gmail.com>
2020-07-29 19:20:37 +0100

    Chore: add SkillsContainer component to MainContainer Component

--- client/src/components/MainContainer/MainContainer.test.js
+++ client/src/components/MainContainer/MainContainer.test.js
@@ -5,6 +5,7 @@ import Profile from '../Profile/Profile';
 import ExperienceContainer from '../Experience/ExperienceContainer/ExperienceContainers';
 import ProjectContainer from '../Projects/ProjectsContainer/ProjectsContainer';
 import EducationContainer from '../Education/EducationContainer/EducationContainer';
+import SkillsContainer from '../Skills/SkillsContainer/SkillsContainer';
 
 describe("MainContainer", () => {
   let wrapper;
@@ -33,6 +34,10 @@ describe("MainContainer", () => {
 
   it("should render a EducationContainer component", () => {
     expect(wrapper.containsMatchingElement(<EducationContainer />)).toEqual(true);
+  });
+
+  it("should render a SkillsContainer component", () => {
+    expect(wrapper.containsMatchingElement(<SkillsContainer />)).toEqual(true);
   });
   
 });
