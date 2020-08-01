a745956318a4a1d11a9513d7ca6692fb2b52e77c
kealanheena <kealanheena@gmail.com>
2020-08-01 22:58:24 +0100

    Chore: add tests for profile component

--- client/src/components/Projects/ProjectCard/ProjectCard.js
+++ client/src/components/Projects/ProjectCard/ProjectCard.js
@@ -4,21 +4,23 @@ import Iframe from '../Iframe/Iframe';
 
 export function ProjectCard({ data }) {
   return (
-    <Card className="h-100 shadow-sm bg-dark text-white rounded">
-      <Iframe displayedWebsite={data.website}/>
-      <Card.Body className="d-flex flex-column">
-        <div className="d-flex mb-2 justify-content-between">
-          <Card.Title className="mb-0 font-weight-bold">
-            {data.title}
-          </Card.Title>
-        </div>
-        <Card.Text className="text-secondary">{data.description}</Card.Text>
-        <Button 
-          href={data.github}
-          variant="success">
-            GitHub
-        </Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card className="h-100 shadow-sm bg-dark text-white rounded">
+        <Iframe displayedWebsite={data.website}/>
+        <Card.Body className="d-flex flex-column">
+          <div className="d-flex mb-2 justify-content-between">
+            <Card.Title className="mb-0 font-weight-bold">
+              {data.title}
+            </Card.Title>
+          </div>
+          <Card.Text className="text-secondary">{data.description}</Card.Text>
+          <Button 
+            href={data.github}
+            variant="success">
+              GitHub
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
