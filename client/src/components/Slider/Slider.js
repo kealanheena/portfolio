e2e649c36ac98e918362cdbfe6dc9857f87cc409
kealanheena <kealanheena@gmail.com>
2020-08-05 21:02:58 +0100

    Chore: finish carousel

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,32 +1,50 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-// import ProjectCard from '../Cards/Project';
+import Iframe from '../Iframe/Iframe';
 import projects from '../../projects.json';
+import { Container, Card, Button } from 'react-bootstrap';
 
 class CenterMode extends Component {
   render() {
     const settings = {
       className: "center",
       centerMode: true,
+      dots: true,
       infinite: true,
       centerPadding: "60px",
-      slidesToShow: 1,
-      speed: 500
+      slidesToShow: 3,
+      speed: 2000,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnHover: true
     };
     return (
       <div>
-        <Slider {...settings}>
-          {projects.map(project => (
-            <div>
-              <h3>{project.id}</h3>
-              <p>{project.title}</p>
-              <p>{project.description}</p>
-              <p>{project.github}</p>
-              <p>{project.website}</p>
-            </div>
-          ))}
+        <Container>
+          <Slider {...settings}>
+            {projects.map(project => (
+              <Container>
+              <Card className="h-100 shadow-sm bg-dark text-white rounded">
+                <Iframe displayedWebsite={project.website}/>
+                <Card.Body className="d-flex flex-column">
+                  <div className="d-flex mb-2 justify-content-between">
+                    <Card.Title className="mb-0 font-weight-bold">
+                      {project.title}
+                    </Card.Title>
+                  </div>
+                  <Card.Text className="text-secondary">
+                    {project.description}
+                  </Card.Text>
+                  <Button href={project.github} variant="success">
+                    GitHub
+                  </Button>
+                </Card.Body>
+              </Card>
+              </Container>
+            ))}
 
-        </Slider>
+          </Slider>
+        </Container>
       </div>
     );
   }
