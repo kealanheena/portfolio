5d7df8a28b6c64b2b707a86f7f90f9de959e1ac9
kealanheena <kealanheena@gmail.com>
2020-08-06 17:51:18 +0100

    Chore: add profile component

--- front-end/src/components/App/App.js
+++ front-end/src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Profile from '../Profile/Profile';
 
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        hello world
+        <Profile/>
       </div>
     );
   }
--- front-end/src/components/App/App.test.js
+++ front-end/src/components/App/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import App from './App';
+import Profile from '../Profile/Profile';
 
 describe('App', () => {
   let wrapper;
@@ -9,6 +11,10 @@ describe('App', () => {
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render a Profile component', () => {
+    expect(wrapper.containsMatchingElement(<Profile/>)).toBe(true)
   });
 
 });
