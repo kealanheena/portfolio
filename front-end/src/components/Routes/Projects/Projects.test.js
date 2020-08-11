11ebc79f1ef2cb117b136bcd1affa92e99712e28
kealanheena <kealanheena@gmail.com>
2020-08-11 23:31:51 +0100

    Chore: add tests for Project component

---
+++ front-end/src/components/Routes/Projects/Projects.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Projects from './Projects';
+import Carousel from './Carousel/Carousel';
+
+
+describe('Projects', () => {
+  let wrapper;
+
+  beforeEach(() => wrapper = shallow(<Projects/>));
+
+  it('should render a <div />', () => {
+    expect(wrapper.find('div').length).toEqual(1)
+  });
+
+  it('should render a Carousel component', () => {
+    expect(wrapper.containsMatchingElement(<Carousel/>)).toEqual(true)
+  });
+
+});
