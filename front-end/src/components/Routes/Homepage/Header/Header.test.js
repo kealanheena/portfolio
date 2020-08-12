e729a305b6834e5ad32040e6637a22b8f37be300
kealanheena <kealanheena@gmail.com>
2020-08-12 16:49:07 +0100

    Chore: test Header component

--- front-end/src/components/Routes/Homepage/Header/Header.test.js
+++ front-end/src/components/Routes/Homepage/Header/Header.test.js
@@ -10,7 +10,19 @@ describe('Header', () => {
   beforeEach(() => wrapper = shallow(<Header/>));
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1)
+    expect(wrapper.find('div').length).toEqual(2)
+  });
+
+  it('should render a <h1 />', () => {
+    expect(wrapper.find('h1').length).toEqual(1)
+  });
+
+  it('should render a <h2 />', () => {
+    expect(wrapper.find('h2').length).toEqual(1)
+  });
+
+  it('should render a <img />', () => {
+    expect(wrapper.find('img').length).toEqual(1)
   });
 
 });
