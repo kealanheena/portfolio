754a29adea40a52a37c4f5575523690d57ae1929
kealanheena <kealanheena@gmail.com>
2020-08-12 23:48:48 +0100

    Chore: Render hobbies component in the homepage

--- front-end/src/components/Routes/Homepage/Homepage.js
+++ front-end/src/components/Routes/Homepage/Homepage.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Profile from './Profile/Profile';
 import Header from './Header/Header';
+import Hobbies from './Hobbies/Hobbies';
 
 function Homepage() {
   return (
     <div>
       <Header/>
       <Profile/>
+      <Hobbies/>
     </div>
   );
 }
--- front-end/src/components/Routes/Homepage/Homepage.test.js
+++ front-end/src/components/Routes/Homepage/Homepage.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 
 import Homepage from './Homepage';
 import Profile from './Profile/Profile';
+import Hobbies from './Hobbies/Hobbies';
 
 describe('Homepage', () => {
   let wrapper;
@@ -15,6 +16,10 @@ describe('Homepage', () => {
 
   it('should render a Profile component', () => {
     expect(wrapper.containsMatchingElement(<Profile/>)).toEqual(true)
+  });
+
+  it('should render a Hobbies component', () => {
+    expect(wrapper.containsMatchingElement(<Hobbies/>)).toEqual(true)
   });
 
 });
