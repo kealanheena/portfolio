aaa77505815affe2974e71e2c16d7c2de7e4bbbf
kealanheena <kealanheena@gmail.com>
2020-08-13 15:09:47 +0100

    Chore: add tests for card and hoobies components

--- front-end/src/components/Card/Card.test.js
+++ front-end/src/components/Card/Card.test.js
@@ -10,11 +10,15 @@ describe('Card', () => {
   beforeEach(() => wrapper = shallow(<Card/>));
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1)
+    expect(wrapper.find('div').length).toEqual(2)
   });
 
-  it('should render a <h2 />', () => {
-    expect(wrapper.find('h2').length).toEqual(1)
+  it('should render a <img />', () => {
+    expect(wrapper.find('img').length).toEqual(1)
+  });
+
+  it('should render a <h4 />', () => {
+    expect(wrapper.find('h4').length).toEqual(1)
   });
 
   it('should render a <p />', () => {
--- front-end/src/components/Routes/Homepage/Hobbies/Hobbies.test.js
+++ front-end/src/components/Routes/Homepage/Hobbies/Hobbies.test.js
@@ -3,15 +3,24 @@ import { shallow } from 'enzyme';
 
 import Hobbies from './Hobbies';
 import HobbiesData from './HobbiesData/HobbiesData';
+import Card from '../../../Card/Card';
 
 describe('Hobbies', () => {
   let wrapper;
 
   beforeEach(() => wrapper = shallow(<Hobbies/>));
 
-  it('should render an amount of <div />\'s equal to the length of HobbiesData plus one', () => {
-    let amountOfDivs = HobbiesData.length + 1;
+  it('should render an amount of <div />\'s equal to the length of HobbiesData plus two', () => {
+    let amountOfDivs = HobbiesData.length + 2;
     expect(wrapper.find('div').length).toEqual(amountOfDivs);
+  });
+
+  it('should render a <h3 />', () => {
+    expect(wrapper.find('h3').length).toEqual(1);
+  });
+
+  it('should render a Card component', () => {
+    expect(wrapper.containsMatchingElement(<Card/>)).toEqual(true)
   });
 
 });
