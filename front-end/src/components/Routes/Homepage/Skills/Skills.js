8fc10087cba36c1c0d1530e5ec4fe2b4efe5c7c5
kealanheena <kealanheena@gmail.com>
2020-08-14 22:02:36 +0100

    Chore: retrieveskills from projects data

--- front-end/src/components/Routes/Homepage/Skills/Skills.js
+++ front-end/src/components/Routes/Homepage/Skills/Skills.js
@@ -1,19 +1,37 @@
 import React from 'react';
 
-import HobbiesData from '../Hobbies/HobbiesData/HobbiesData';
+import CarouselData from '../../../Carousel/CarouselData/CarouselData';
 import Card from '../../../Card/Card';
 
 import './Skills.css'
 
-function Hobbies() {
+function Skills() {
+
+  function getAllSkills({ data }) {
+    let arr = []
+    CarouselData.forEach( item => 
+      arr = arr.concat(item.stack).sort()
+    )
+    return arr;
+  }
+
   return(
     <div className="spacing">
       <h2>Skills</h2>
-      <div className="hobbies-container">
-        
+      {getAllSkills()}
+      <div className="skills-container">
+        {CarouselData.map( hobby  =>
+          <div key={hobby.id} className="card-container">
+            <Card
+              id={hobby.id}
+              title={hobby.title}
+              content={hobby.description}
+            />
+          </div>
+        )}
       </div>
     </div>
   )
 }
 
-export default Hobbies
+export default Skills
